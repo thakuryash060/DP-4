21e3349ef2081b061d5ed6ffeed3d8971aa62769
Yash Thakur <144375330+thakuryash060@users.noreply.github.com>
2025-08-17 02:03:07 +0530

    Create Problem1.java
    Time Complexity : O(m*n)
    Space Complexity : O(n)

---
+++ Problem1.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        if(matrix == null || matrix.length==0){
+            return -1;
+        }
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int max = 0;
+        int []dp = new int[n+1];
+        int temp = dp[0];
+       
+        for(int i = 1 ; i< m+1 ; i++){
+            
+            for(int j = 1 ; j< n+1 ; j++){
+                int prev = dp[j];
+                if(matrix[i-1][j-1]=='1'){
+                    dp[j]=1+Math.min(dp[j-1],Math.min(temp,dp[j]));
+                    max = Math.max(max,dp[j]);
+                }
+
+                else{
+                    dp[j]=0;
+                }
+
+                temp = prev;
+            }
+        }
+        return max*max;
+    }
+}
